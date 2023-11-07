7a6880ea2b1352ae471e1766284855fd61781a4c
NJ-Zerin <nusratzerin44@gmail.com>
2023-11-07 15:36:21 +0600

    changes in company profile module file

--- src/company/company_profile/company_profile.module.ts
+++ src/company/company_profile/company_profile.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CompanyProfileController } from './company_profile.controller';
+import { CompanyProfileService } from './company_profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyProfileEntity } from './company_profile.entity';
 
 @Module({
-    imports : [],
+    imports : [TypeOrmModule.forFeature([CompanyProfileEntity]),],
     controllers : [CompanyProfileController],
+    providers:[CompanyProfileService],
 
 })
 export class CompanyProfileModule {}
